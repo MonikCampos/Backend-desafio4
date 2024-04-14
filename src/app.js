929ebac3318a7a2ae57988b2a9c537bce5640ba0
monicampos <monica.alejandra.campos@gmail.com>
2024-04-13 23:11:20 -0300

    express handlebars

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import express  from 'express';
+//import {Server} from 'socket.io';
+import { engine } from 'express-handlebars';
+
 import { router as productsRouter } from "./routes/productsRouter.js"
 import { router as cartsRouter } from "./routes/cartsRouter.js";
+import __dirname from "./utils.js";
+
+
 
 const port=8080;
 const app=express();
@@ -8,11 +14,16 @@ const app=express();
 //App.use para utilizar los routers
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname +  '/public')); //Para que se puedan ver las imagenes y estilos
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+
 
 //rutas
 app.get( "/", (req, res) => {
-    const message = `Primera Pre-Entrega!`;
-    res.status(200).send({message});
+    return res.render("home");
 })
 app.use("/api/products", productsRouter)
 app.use("/api/carts", cartsRouter)
